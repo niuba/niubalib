0edcbfe44b82d868ca573819783a97e3cfc22eb6
niuba <niuba.zbz@gmail.com>
2018-06-27 13:56:25 +0800

    first commit
    Signed-off-by: niuba <niuba.zbz@gmail.com>

---
+++ types/wepy/index.d.ts
@@ -0,0 +1,29 @@
+import component from "./component";
+import mixin from "./mixin";
+import event from "./event";
+import page from "./page";
+import app from "./app";
+import util from "./util";
+import base from "./base";
+import { WxEnhances } from "./wx_enhanced";
+
+interface DefaultExport extends WxEnhances {
+    event: typeof event;
+    app: typeof app;
+    component: typeof component;
+    page: typeof page;
+    mixin: typeof mixin;
+    $createApp: typeof base.$createApp;
+    $createPage: typeof base.$createPage;
+    $isEmpty: typeof util.$isEmpty;
+    $isEqual: typeof util.$isEqual;
+    $isDeepEqual: typeof util.$isDeepEqual;
+    $has: typeof util.$has;
+    $extend: typeof util.$extend;
+    $isPlainObject: typeof util.$isPlainObject;
+    $copy: typeof util.$copy;
+}
+
+declare const defaultExport: DefaultExport;
+
+export default defaultExport;
